721b253677b6c2e4ab322a227faf9b93647dd499
Puzakou Maksim <puzakou.m@gmail.com>
2023-04-21 12:19:12 +0300

    Create script.js

---
+++ lesson_12/script.js
@@ -0,0 +1,5 @@
+let first_name = prompt('Ваше имя');
+let last_name = prompt('Ваша фамилия?');
+let sex = prompt('Ваш пол?');
+let age = prompt('Ваш возраст?')
+alert(first_name + "\n" + last_name + "\n" + sex + "\n" + age);
